e4553cdf7980a5a1dc7bf71a92cac5986a1ba9f8
Will S <fitnesswill13@gmail.com>
2022-12-04 14:46:45 -0800

    modal added to validating email

--- portfolio/src/components/pages/Contact.js
+++ portfolio/src/components/pages/Contact.js
@@ -3,38 +3,40 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { validateEmail } from '../../utils/helpers';
 import emailjs from '@emailjs/browser';
+import Modal from 'react-bootstrap/Modal';
 
 export default function Contact() {
-  
+
   const [formState, setFormState] = useState({ name: "", email: "", message: "" });
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const handleInputChange = (e) => {
     console.log(e.target.name)
     console.log(e.target.value)
-    // if (e.target.name === 'email') {
-    //   validateEmail(e.target.value);
-    // }
-    setFormState({...formState, [e.target.name]: e.target.value});
+    setFormState({ ...formState, [e.target.name]: e.target.value });
     console.log(formState)
 
   };
   const handleFormSubmit = (e) => {
     e.preventDefault();
     console.log(formState.email)
-   
-      console.log(validateEmail(formState.email))
-      if (!validateEmail(formState.email) || !formState.message || !formState.name){
-        alert("Please ensure your email is valid and all fields are filled out so that I can get back to you!")
-      }
 
-    setFormState({email: "",name: "",message: ""})
+    console.log(validateEmail(formState.email))
+    if (!validateEmail(formState.email) || !formState.message || !formState.name) {
+      return handleShow()
+    }
+    setFormState({ email: "", name: "", message: "" })
 
     emailjs.send('default_service', 'template_dsjx25m', formState, 'C8S5M9CyzbHsWjtS2')
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    });
+      .then(function (response) {
+        console.log('SUCCESS!', response.status, response.text);
+      }, function (error) {
+        console.log('FAILED...', error);
+      });
+
   };
   return (
     <div style={{ maxWidth: "1000px", margin: "0 auto" }}>
@@ -51,25 +53,36 @@ export default function Contact() {
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control 
-          value={formState.email}
-          name="email"
-          onChange={handleInputChange}
-          type="email" placeholder="Enter email" />
+          <Form.Control
+            value={formState.email}
+            name="email"
+            onChange={handleInputChange}
+            type="email" placeholder="Enter email" />
         </Form.Group>
         <Form.Group controlId="formBasicMessage">
           <Form.Label>Message Me</Form.Label>
-          <Form.Control 
-          value={formState.message}
-          name="message"
-          onChange={handleInputChange}
-          as="textarea" 
-          aria-label="With textarea" />
+          <Form.Control
+            value={formState.message}
+            name="message"
+            onChange={handleInputChange}
+            as="textarea"
+            aria-label="With textarea" />
         </Form.Group>
         <Button variant="primary" onClick={handleFormSubmit} type="submit">
           Submit
         </Button>
       </Form>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Uhh Oh!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Please complete all fields with a valid email address.  Thanks!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
--- portfolio/src/components/pages/Portfolio.js
+++ portfolio/src/components/pages/Portfolio.js
@@ -2,6 +2,11 @@ import React from 'react';
 import Card from './Card';
 
 export default function Blog() {
+
+  const activeLinks = [" https://wseban.github.io/Mickey-n-Friends-Finder/", "https://wseban.github.io/Weather-Dashboard/", "https://wseban.github.io/Day-Schedule/", "", "", ""]
+const repoLinks = ["https://github.com/wseban/Mickey-n-Friends-Finder", "https://github.com/wseban/Weather-Dashboard", "https://github.com/wseban/Day-Schedule", "https://github.com/wseban/Get-Route", "https://github.com/wseban/Note-Taker", "https://github.com/wseban/Code-Quiz"];
+
+
   return (
     <div>
       <h2>Portfolio</h2>
